7a8f194a8035cf1af5caf738f947d027caf88cc2
KevshuppD <kcastrod@uft.edu>
2026-06-12 23:59:34 -0400

    clean: Remove temporary scratch files and logs to clean workspace

--- scratch/check_tags.py
+++
@@ -1,9 +0,0 @@
-import sys
-try:
-    from mutagen.flac import FLAC
-    audio = FLAC("scratch/06. Labrinth - Nate Growing Up.flac")
-    print("Pictures count:", len(audio.pictures))
-    for i, pic in enumerate(audio.pictures):
-        print(f"Picture {i}: mime={pic.mime}, type={pic.type}, desc={pic.desc}, size={len(pic.data)} bytes")
-except Exception as e:
-    print("Error:", e)
