93ef57312a77b5007f4ea126dfa77d261d617df6
Kevshupp <kevshupp.musicplayer@gmail.com>
2026-06-13 21:09:35 -0400

    Bump version to 10 (1.2.4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.kevshupp.kevmusicplayer"
         minSdk = 33
         targetSdk = 37
-        versionCode = 9
-        versionName = "1.2.3"
+        versionCode = 10
+        versionName = "1.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
