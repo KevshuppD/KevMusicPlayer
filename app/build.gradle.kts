0a43b8970c7c4fc2994598b8c14bf993a2139bd9
KevshuppD <KevshuppD@users.noreply.github.com>
2026-08-21 01:22:23 -0400

    chore: bump version to v1.2.23 (modularizacion arquitectonica y buscador de canciones cortas)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.kevshupp.kevmusicplayer"
         minSdk = 33
         targetSdk = 37
-        versionCode = 27
-        versionName = "1.2.22"
+        versionCode = 28
+        versionName = "1.2.23"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
